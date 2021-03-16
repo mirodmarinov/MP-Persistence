e03d92695ee8b55222574d730d9925b57390faab
martinko-boop <75024378+martinko-boop@users.noreply.github.com>
2021-03-16 11:20:33 +0100

    Creating test class
    only TestDBConnection

---
+++ src/test/TestDBConnection.java
@@ -0,0 +1,27 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.Connection;
+
+import org.junit.jupiter.api.Test;
+
+import database.DBConnection;
+
+class TestDBConnection
+{
+
+	@Test
+	void testGetConnection() 
+	{
+		try 
+		{
+			Connection connection = DBConnection.getInstance().getConnection();
+			assertNotNull(connection);
+		} 
+		catch(Exception e)
+		{
+			fail("Issues with DBConnection");
+		}	
+	}
+}
