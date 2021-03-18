180fe43daf3b744f419670defeba1db546a77e04
WrexBG <40688355+WrexBG@users.noreply.github.com>
2021-03-18 13:40:53 +0100

    Update Customer.java

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -9,8 +9,13 @@ public class Customer
 	private String phoneNumber;
 	private Type   type;
 	
-	public Customer(String name, String address)
+	public Customer(String name, String address, String zipcode, String city, String phoneNumber, Type type)
 	{
-		
+		this.name = name;
+		this.address = address;
+		this.zipcode = zipcode;
+		this.city = city;
+		this.phoneNumber = phoneNumber;
+		this.type = type;
 	}
 }
