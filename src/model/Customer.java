f01f75dbe1994119f34767ca7060800021ab52e4
WrexBG <40688355+WrexBG@users.noreply.github.com>
2021-03-18 13:33:44 +0100

    Update Customer.java

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,5 +1,16 @@
 package model;
 
-public class Customer {
-
+public class Customer
+{
+	private String name;
+	private String address;
+	private String zipcode;
+	private String city;
+	private String phoneNumber;
+	private Type   type;
+	
+	public Customer(String name, String address)
+	{
+		
+	}
 }
