3d4bf14667ac84ce5b1de812cb729e3964beca09
WrexBG <40688355+WrexBG@users.noreply.github.com>
2021-03-19 13:35:39 +0100

    Comments to control layer

--- src/control/CustomerController.java
+++ src/control/CustomerController.java
@@ -5,22 +5,33 @@ import java.sql.SQLException;
 import database.CustomerDB;
 import model.Customer;
 
+/**
+ * @author Group1 dmai0920
+ * Handles the customer logic
+ */
 public class CustomerController
 {
+	//Fields
 	private CustomerDB customerDB; 
 
+	/**
+	 * General constructor setting the fields
+	 * @throws SQLException
+	 */
 	public CustomerController() throws SQLException //TODO - check exception
 	{
 		customerDB = new CustomerDB();
 	}
 	
+	/**
+	 * Method to find the customer by phone
+	 * @param phone
+	 * @return
+	 * @throws SQLException
+	 */
 	public Customer findCustomerByPhone(String phone) throws SQLException //TODO - check exception
 	{
-		Customer customer = null;
-		
-		customer = customerDB.findCustomerByPhone(phone);
-		
-		return customer;
+		return customerDB.findCustomerByPhone(phone);
 	}
 
 }
--- src/control/OrderController.java
+++ src/control/OrderController.java
@@ -4,15 +4,30 @@ import java.sql.SQLException;
 
 import model.Customer;
 
+/**
+ * @author Group1 dmai0920
+ * Handles the order logic
+ */
 public class OrderController
 {
+	//All fields
 	CustomerController customerCtr;
 
+	/**
+	 * General constructor setting the fields
+	 * @throws SQLException
+	 */
 	public OrderController() throws SQLException //TODO check exception
 	{
 		customerCtr = new CustomerController();
 	}
 
+	/**
+	 * A method to get a customer by phone from the Customer controller
+	 * @param phone
+	 * @return
+	 * @throws SQLException
+	 */
 	public String[] findCustomerByPhone(String phone) throws SQLException //TODO fix exception
 	{
 		String[] customerInfo;
